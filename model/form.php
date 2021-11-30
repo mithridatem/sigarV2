4fb5628fc97e80263efcc295c32928982c27c3aa
mathieu <hcm81informatique@hotmail.fr>
2021-11-30 19:49:49 +0100

    connexion form files

---
+++ model/form.php
@@ -0,0 +1,15 @@
+<?php
+    class Formateur{
+        private $id_form;
+        private $name_form;
+        private $first_name_form;
+        private $pseudo_form;
+        private $mdp_form;
+        
+
+
+    }
+
+
+
+?>
