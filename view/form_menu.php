f1ddb17f7d52463bc0764146fc2138972ed4d0df
Mathieu Mithridate <mathieumithridate@macbook-air-de-mathieu.home>
2021-12-31 21:53:56 +0100

    update css & view view_form_show_seance.php

--- view/form_menu.php
+++ view/form_menu.php
@@ -2,7 +2,7 @@
     <nav>
         <div class="subnav">
             <img src="./img/menu.svg"  class="burgerButton" width="40px">
-            <div id="tag">
+            <div id="label">
                 <h1>SIGAR Formateur</h1>
                 <img src="./img/logonum.png" id="securise"> 
             </div>
