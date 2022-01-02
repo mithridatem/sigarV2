ee1dd746e4905f13e03048ad23ee319452b93043
Mathieu Mithridate <mathieumithridate@macbook-air-de-mathieu.home>
2022-01-02 04:16:22 +0100

    update

--- form_show_absence.php
+++ form_show_absence.php
@@ -36,8 +36,15 @@
         //$currentAbs = $cours->showAbscurrentMonth($bdd, $tag, $dateDeb, $dDay);
         $currentAbs = $cours->showAllAbsByMonth($bdd, $tag, $numMonth);
         $pastAbs = $cours->showAllAbsByMonth($bdd, $tag, $numMonthPre);
+        if($numMonth == 1){
+            $pastAbs = $cours->showAllAbsByMonth($bdd, $tag, 12);
+            $currentAbs = $cours->showAbscurrentMonth($bdd, $tag, $dateDeb, $dDay);
+            echo '<p>Nombres d\'absences (demi-journées) : </p>';
+            echo '<p>Session : '.$tag.' Absences totales du mois prédédent : <span>'.$pastAbs.'</span> 
+            Absences totale du mois de '.$tabMonth[0].' : <span id="alert">'.$currentAbs.'</span></p>';
+        }
         //test si le mois est le mois courant affichage du nombre d'absence du 1 à la date du jours
-        if($numMonth == $month){
+        else if($numMonth == $month){
             $currentAbs = $cours->showAbscurrentMonth($bdd, $tag, $dateDeb, $dDay);
             echo '<p>Nombres d\'absences (demi-journées) : </p>';
             echo '<p>Session : '.$tag.' Absences totales du mois prédédent : <span>'.$pastAbs.'</span> 
@@ -66,6 +73,7 @@
         $numMonth = date('m');
         $datedeb = "$year-$numMonth-01";
         $cours->showAbscurrentMonthAll($bdd, $datedeb, $dDay);
+        //$cours->showAbscurrentMonthAll($bdd, '2022-01-03', '2022-01-06');
    }
 
 ?>
